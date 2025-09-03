9847365a153f06434edc9e32988aa1bd7e53e1ee
MickaelLepicier <mickael233@gmail.com>
2025-09-03 16:53:59 +0300

    feat: add welcome, download CV button and  images

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,60 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar'
+import hi from '../../assets/hi.png'
+import CV from './CV.pdf'
+import img from '../../assets/img.png'
 
 export default function Hero() {
   return (
     <div className="relative overflow-hidden min-h-[550px] sm:min-h-[660px] flex flex-col">
-      <div className="md:h-[720px] h-[960px] md:w-[640px] w-[880px] right-0 bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400 absolute rounded-full transform rotate-12 -top-20 shadow-[0_0_40px_rgb(255, 165, 0, 0.7)] "></div>
+      {/* <div className="md:h-[720px] h-[960px] md:w-[640px] w-[880px] right-0 bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400 absolute rounded-full transform rotate-12 -top-20 shadow-[0_0_40px_rgb(255, 165, 0, 0.7)] "></div> */}
+
+      <div
+        className="absolute 
+                w-[720px] h-[720px]    
+                md:w-[640px] md:h-[640px] 
+                right-0 -top-20
+                bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400
+                rounded-full             
+                transform rotate-12
+                shadow-[0_0_40px_rgb(255,165,0,0.4)] "
+      ></div>
+
       <Navbar />
+      <section className="text-white body-font z-10">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center ">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center relative">
+            <img
+              src={hi}
+              className="absolute top-[350px] left-44 md:top-[30px] md:left-[450px] transform-translate-x-1/2 -translate-y-1/2 w-20 h-20 "
+            />
+            <h1 className="title-font sm:text-4xl text-4xl mb-4 font-bold text-white">
+              Hi! I'm Mickael
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
+              natus fuga error pariatur tempore, asperiores dolor quo facilis
+              consequuntur expedita inventore. Blanditiis at quae eum minima
+              perspiciatis, vel aut laudantium.
+            </p>
+
+            <div className="flex justify-center">
+              <a href={CV} download>
+                <button className="inline-flex text-white bg-orange-500 border-0 py-2 px-6 focus:outline-none hover:bg-orange-600 hover:shadow-[0_0_40px_rgb(255,165,0,0.7)] rounded-full text-lg">
+                  Download CV
+                </button>
+              </a>
+            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              src={img}
+              alt="hero"
+              className="object-cover object-center rounded-full w-80 h-80"
+            />
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
