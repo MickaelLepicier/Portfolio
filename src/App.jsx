1adafdcb3f37bb52031b97a295e27d72f79d5da6
MickaelLepicier <mickael233@gmail.com>
2025-09-07 13:48:42 +0300

    feat: create experience comp and add img_experience image

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 import Hero from './components/hero/Hero'
 import Skills from './components/skills/Skills'
+import Experience from './components/experience/Experience'
 
 function App() {
   useEffect(() => {
@@ -14,6 +15,7 @@ function App() {
     <main className="bg-[#0d182e]">
       <Hero />
       <Skills />
+      <Experience />
     </main>
   )
 }
